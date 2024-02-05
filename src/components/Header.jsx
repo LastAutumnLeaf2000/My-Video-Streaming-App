901942c0c976b9f4f7dd1823017ab5e6b49eb97c
LastAutumnLeaf2000 <pinunileshjadhav2000@gmail.com>
2024-02-06 01:06:38 +0530

    BBQ

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -90,7 +90,6 @@ const Header = () => {
               🔍
             </button>
           </form>
-          {/** */}
           {showSuggestions && searchQuerry ? (
             <div className="absolute right-[6.5rem] lg:left-[29.6rem] mt-1 bg-white shadow-xl w-[12rem] lg:w-[37rem] text-start rounded-xl overflow-hidden border border-gray-100">
               <ul className="my-2" >
